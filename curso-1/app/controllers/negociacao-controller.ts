5122723c5b8fdcc3a7aa7d94f28ebc05f45f9df4
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-11 17:57:05 -0300

    tipando atributos

--- curso-1/app/controllers/negociacao-controller.ts
+++ curso-1/app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
+import { Negociacao } from "../models/negociacao.js";
+
 // criando a classe
 export class NegociacaoController {
-    // criando atributos
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor;
+    // criando atributos tipados
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
 
     // criando construtor que percorre arvore DOM
     constructor() {
@@ -14,8 +16,15 @@ export class NegociacaoController {
 
     // criando funcao para adicionar
     adiciona() {
-        console.log(this._inputData);
-        console.log(this._inputQuantidade);
-        console.log(this._inputValor);
+        //criando expressão regular para selcionar hifens
+        const exp = /-/g;
+        // convertendo elementos html nos tipos corretos
+        const date = new Date(this._inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this._inputQuantidade.value);
+        const valor = parseFloat(this._inputValor.value);
+        // instanciando objeto
+        const negociacao = new Negociacao(date, quantidade, valor);
+
+        console.log(negociacao)
     }
 }
